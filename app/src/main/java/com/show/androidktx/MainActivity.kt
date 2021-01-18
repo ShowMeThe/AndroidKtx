28aaac5b8e0fe5047dda8fe7dc848ed516b96b9a
ken <c.uvsuv@163.com>
2021-01-18 18:42:31 +0800

    update MainActivity.kt

--- app/src/main/java/com/show/androidktx/MainActivity.kt
+++ app/src/main/java/com/show/androidktx/MainActivity.kt
@@ -2,13 +2,14 @@ package com.show.androidktx
 
 import android.graphics.Color
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.show.androidktx.databinding.ActivityMainBinding
 import com.show.binding.BindingProperty
 import com.show.binding.binding
-import com.show.span.foregroundColor
-import com.show.span.span
+import com.show.span.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,13 +24,25 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
 
+            tv1.movementMethod = LinkMovementMethod.getInstance()
+            tv1.setHintTextColor(Color.TRANSPARENT)
             tv1.text = text.span.apply {
                 foregroundColor(Color.RED,0..7)
-
+                italic(0..2)
+                backgroundColor(Color.LTGRAY,0..3)
+                underline(0..2)
+                strikeThrough(6..9)
+                bold(2..4)
+                boldItalic(6..9)
+                size(25,0..1)
+                sizeRelative(2f,1..2)
+                val drawable = ContextCompat.getDrawable(this@MainActivity,R.mipmap.ic_launcher_round)
+                drawable!!.setBounds(0,0,drawable.intrinsicHeight,drawable.intrinsicWidth)
+                image(drawable,0..1)
+                click(10..12){
+                    Toast.makeText(this@MainActivity, text,Toast.LENGTH_LONG).show()
+                }
             }
-
-
-
 
         }
 
